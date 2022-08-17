f796947148af01cd3b4a006636fd40680211a5f3
Alexandru Fedin <alexandrufedin4@gmail.com>
2022-08-17 17:57:20 +0300

    Legatura cu celelalte failuri

---
+++ calculatorCredite.js
@@ -0,0 +1,20 @@
+document.querySelector(".verify1").addEventListener("click", function() {
+  const loanAmount = document.querySelector(".inputLoanAmount").value;
+  const loanTerm = (document.querySelector(".inputLoanTerm").value);
+  const interestRate = (document.querySelector(".inputInterestRate").value);
+  if ((loanAmount == '') || (loanTerm == '') || (interestRate == '')) {
+    document.querySelector('.result1').innerHTML = `Please enter all the data.`
+  }
+  //parseInt = transform a string into a number
+  else {
+    let nrSumei = Number(loanAmount);
+    let nrLuni = Number(loanTerm);
+    let procentDobanda = Number(interestRate);
+    sumatLunaraDobanda = (nrSumei * procentDobanda) / 100;
+    let sumaTotalaDobanda = sumatLunaraDobanda + nrLuni ;
+    let sumaLunaraDobanda = sumaTotalaDobanda  / nrLuni;
+    let sumaTotalaCredit = sumaTotalaDobanda + nrSumei;
+    let achitareLuna = sumaTotalaCredit / nrLuni;
+    document.querySelector('.result1').innerHTML = `Suma totala a dobanzii este <b>${sumaTotalaDobanda}</b> iar suma lunara a dobanzii este <b>${sumaLunaraDobanda}</b>. <br> Suma totala pentru achitarea creditului la final este <b>${sumaTotalaCredit}</b>. <p><p> Va trebui sa platiti lunar <b>${achitareLuna}</b>.</p></p>`;
+  }
+});
